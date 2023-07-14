b72fc556451651059b748e3c71625c4a34fb4411
ryogo <3obiarmadillo@gmail.com>
2023-07-14 17:03:47 +0900

    anything

---
+++ dotfilesLink.sh
@@ -0,0 +1,8 @@
+ #!/bin/sh
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/colors ~/.vim
+ln -sf ~/dotfiles/template ~/.vim
+ln -sf ~/dotfiles/dein.toml ~/.vim
+ln -sf ~/dotfiles/dein_lazy.toml ~/.vim
+
